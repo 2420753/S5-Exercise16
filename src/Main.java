50ee18d61182223caa91525f0396dab582d5fa6d
Abdulmalik Elghedafi <2420753@uwtsd@ac.uk>
2024-10-29 13:47:40 +0000

    completed exercise

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,11 @@ public class Main {
     public static final Scanner stdin = new Scanner(System.in);
     public static void main(String[] args) {
 
-
-
         System.out.println("Welcome to the treasure hunt game!");
 
-        System.out.print("Enter column size: ");
-        int columnSize = Integer.parseInt(stdin.nextLine());
-
-        System.out.print("Enter row size: ");
-        int rowSize = Integer.parseInt(stdin.nextLine());
+        // Setting row and column sizes
+        int columnSize = 5;
+        int rowSize = 3;
         int[][] array = new int[columnSize][rowSize];
 
         // setting the random variable
@@ -23,16 +19,21 @@ public class Main {
 
         // Assigning the selected cell to 1:
         array[randomColumn][randomRow] = 1;
+        // new line
+        System.out.print("\n");
+        // Asking user to guess the column and row sizes
+        System.out.print("Please guess column size: ");
+        int guessColumnSize = stdin.nextInt();
 
+        System.out.print("Please guess row size: ");
+        int guessRowSize = stdin.nextInt();
 
+        // Logic for the program
 
-
-
-
-        // iterating over the rows and column
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-            }
+        if (randomColumn == guessColumnSize && randomRow == guessRowSize) {
+            System.out.println("Congratulations! You win!");
+        } else {
+            System.out.println("Sorry, you lost!");
         }
 
         // assigning the value to each row and column
